a6b6be7e5ed81fb49653f2516168c48e1a0fe34a
Arthur Hwang <arthurhwangs@gmail.com>
2020-11-01 12:31:02 -0500

    added html and mod app

--- app.py
+++ app.py
@@ -2,14 +2,36 @@
 Simple "Hello, World" application using Flask
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
+from mbta_helper import find_stop_near
 
 
 app = Flask(__name__)
 
+@app.route("/")
+def index():
+    return render_template("index.html")
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
 
-hello_world()
+@app.route('/nearest/', methods=['GET','POST'])
+
+def nearest():
+
+    if request.method == "POST":
+        place_name = str(request.form['location'])
+        mbta_station = find_stop_near(place_name)
+        station = mbta_station[0]
+        wheelchair_accessable = mbta_station[1]
+
+        if mbta_station:
+            return render_template(
+                "return.html",
+                place_name = place_name,
+                station = station,
+                wheelchair_accessable = wheelchair_accessable
+            )
+        else:
+            return render_template("form.html", error = True)
+
+    return render_template("form.html", error = True)
+
